e2d0f2c23438501b179560e6458693535a94ef92
Sunying-RONG <sunying.rong@hotmail.com>
2021-11-04 16:07:39 +0100

    update

--- ScriptInsertion.sql
+++ ScriptInsertion.sql
@@ -63,16 +63,25 @@ COMMIT;
 -- SELECT * FROM EMP;
 
 -- 1 Donner les nom, fonction et date d’embauche de tous les employés,
+-- projection
+
 -- SELECT nom, fonction, embauche from EMP;
 
+
 -- 2. Donner les numéros, nom et salaire des employés dont le salaire est <= 2000 euros,
+-- selection, projection
+
 -- SELECT num, nom, salaire from EMP 
 -- WHERE salaire <= 2000;
 
+
 -- 3. Donner la liste des employés ayant une commission, classée par commission décroissante,
+-- selection, order by, projection
+
 -- SELECT nom FROM EMP
 -- WHERE comm IS NOT NULL
 -- ORDER BY comm DESC;
+
 
 -- retourner les departements qui ont plus de 5 employées
 -- SELECT D.n_dept, D.nom, count(E.num)
@@ -95,3 +104,113 @@ COMMIT;
 -- SELECT *
 -- FROM EMP
 -- ORDER BY n_dept, num;
+
+-- 4. Donner le nom des personnes embauchées depuis janvier 1991,
+-- For year use YYYY to compare, more clear
+-- condition, projection
+
+-- SELECT nom 
+-- FROM EMP 
+-- WHERE embauche > TO_DATE('01-JAN-1991','DD-MON-YYYY');
+
+
+-- 5. Donner pour chaque employé son nom et son lieu de travail,
+-- jointure naturelle, projection
+
+-- SELECT e.nom, d.lieu 
+-- FROM EMP e, DEPT d 
+-- WHERE e.n_dept = d.n_dept;
+
+
+-- 6. Donner pour chaque employé le nom de son supérieur hiérarchique,
+-- auto jointure, projection
+-- SELECT e1.nom AS superieur, e2.nom 
+-- FROM EMP e1, EMP e2 
+-- WHERE e1.num = e2.n_sup;
+
+-- 7. Quels sont les employés ayant la même fonction que ”CODD” ?
+-- projection, selection imbrique
+-- SELECT nom FROM EMP WHERE fonction = (SELECT fonction FROM EMP WHERE nom = 'CODD');
+
+
+-- 8. Quels sont les employés gagnant plus que tous les employés du département 30 ?
+-- projection, selection imbrique, max()
+
+-- SELECT nom FROM EMP WHERE salaire > (SELECT max(salaire) FROM EMP WHERE n_dept = 30);
+
+
+-- 9. Quels sont les employés ne travaillant pas dans le même département que leur supérieur hiérarchique ?
+-- projection, selection, jointure
+
+-- SELECT e1.nom 
+-- FROM EMP e1, EMP e2 
+-- WHERE e1.n_sup = e2.num AND e1.n_dept != e2.n_dept;
+
+
+-- 10. Quels sont les employés travaillant dans un département qui a procédé à des embauches depuis le début de l’année 98,
+-- select distinct imbrique, projection, 
+
+-- SELECT nom 
+-- FROM EMP 
+-- WHERE n_dept IN (SELECT DISTINCT n_dept FROM EMP WHERE embauche > TO_DATE('01-JAN-1998', 'DD-MON-YYYY'));
+
+
+-- 11. Donner le nom, la fonction et le salaire de l’employé (ou des employés) ayant le salaire le plus élevé,
+-- projection, selection imbrique, max()
+
+-- SELECT nom, fonction, salaire
+-- FROM EMP
+-- WHERE salaire = (SELECT max(salaire) FROM EMP);
+
+
+-- 12. Donner le total des salaires, le nombre de salariés, ainsi que le salaire minimal, moyen et maximal pour l’ensemble des salariés de chaque département,
+-- projection, GROUP BY,  unction
+
+-- SELECT n_dept, sum(salaire), count(num), min(salaire), avg(salaire), max(salaire) 
+-- FROM EMP 
+-- GROUP BY n_dept;
+
+
+-- 13. Donner le ou les départements ayant le plus d’employés,
+-- create vie
+
+-- CREATE OR REPLACE VIEW ChaqueDept AS
+-- SELECT n_dept, count(num) AS nombre 
+-- FROM EMP 
+-- GROUP BY n_dept;
+
+-- SELECT n_dept
+-- FROM ChaqueDept
+-- WHERE nombre = (SELECT max(nombre) FROM ChaqueDept);
+
+
+-- 14. Donner les départements qui ne possèdent pas d’employés exerçant la fonction d’ingénieur,
+-- MINUS
+
+-- SELECT n_dept
+-- FROM DEPT
+-- MINUS
+-- SELECT n_dept
+-- FROM EMP 
+-- WHERE fonction = 'ingenieur';
+
+
+-- 15. Donner les départements possédant des employés exerçant l’ensemble des fonctions référencées au sein de la société.
+-- division, par deux difference
+CREATE OR REPLACE VIEW NonToutFonc AS
+SELECT DISTINCT e1.n_dept, e2.fonction 
+FROM EMP e1, EMP e2
+MINUS 
+SELECT DISTINCT n_dept, fonction
+FROM EMP;
+
+SELECT DISTINCT n_dept
+FROM EMP 
+MINUS 
+SELECT n_dept
+FROM NonToutFonc;
+
+-- CHECK
+SELECT n_dept, fonction
+FROM EMP 
+ORDER BY n_dept;
